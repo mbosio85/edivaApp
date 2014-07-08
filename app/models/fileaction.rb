ab5fb48854d2563af97af2e8f60a2fb82e785f5e
rubayte <rubayte.iut@gmail.com>
2014-07-08 16:12:43 +0200

    pymailer deleted

--- app/models/fileaction.rb
+++ app/models/fileaction.rb
@@ -4,40 +4,26 @@ class Fileaction
     
   end
 
-<<<<<<< HEAD
+
   def self.showFile(file,user)
     
     data = Array.new
     
-    File.open(Rails.root.join(user,file),'r') do |line|
-=======
-  def self.showFile(file,user,project)
-    
-    data = Array.new
-    
     File.open(Rails.root.join(user,project,file),'r') do |line|
->>>>>>> 125617c60d28ff78cc6dfcac741e9583c13b493f
       data.push(line)
     end
     
     return data,"show"    
   end
 
-<<<<<<< HEAD
   def self.deleteFile(file,user)
     
   end
 
   def self.downloadFile(file,user)
-=======
-  def self.deleteFile(file,user,project)
     
   end
 
-  def self.downloadFile(file,user,project)
->>>>>>> 125617c60d28ff78cc6dfcac741e9583c13b493f
-    
-  end
 
 
 end
