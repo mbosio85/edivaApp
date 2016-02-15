adf69ed9ff882a0abf787f5efaaf154b2ef265f1
mbosio <mattia.bosio85@gmail.com>
2016-02-15 17:40:30 +0100

    attempt reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -165,7 +165,7 @@ class User
         
         
               
-        mailCmd = "/home/rrahman/soft/ts-0.7.5/ts -N 1 python /home/rrahman/soft/python-mailer/pymailer.py -s /home/rrahman/soft/python-mailer/newpass.html userspace/"+uname+'/.csv_file.csv  ediva_new_password:'+pass+"\n"
+        mailCmd = "/home/rrahman/soft/ts-0.7.5/ts -N 1 python /home/rrahman/soft/python-mailer/pymailer.py -s /home/rrahman/soft/python-mailer/newpass.html userspace/"+uname+'/.csv_file.csv  ediva_new_password:'+newpass+"\n"
         system(mailCmd)
         return "validuser"
         
